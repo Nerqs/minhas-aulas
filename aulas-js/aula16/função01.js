780bb87881577d1095c6b72fdbf99b47c41ee7dd
Jean Carlos <jeancpereira@outlook.com.br>
2022-01-27 18:51:07 -0300

    +aulas

---
+++ aulas-js/aula16/função01.js
@@ -0,0 +1,11 @@
+function parimpar(n) {
+    if (n%2 == 0) {
+        return 'Par'
+    } else {
+        return 'Impar'
+    }
+}
+
+//var res = parimpar(57)
+//console.log(res)
+console.log(parimpar(4))
