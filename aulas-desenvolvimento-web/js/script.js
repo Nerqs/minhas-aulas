9af6cb248cd1b777240c676a9af4144a415befc8
Jean Carlos <jeancpereira@outlook.com.br>
2022-02-13 11:48:55 -0300

    aulas

--- aulas-desenvolvimento-web/js/script.js
+++
@@ -1,9 +0,0 @@
-function autorizar() {
-    let num = document.getElementById('num')
-    let nome = document.getElementById('nome')
-    if (num.value == 2) {
-        nome.disabled = false
-    } else {
-        nome.disabled = true
-    }
-}
